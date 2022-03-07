58c3b427d1066fbb2dbc18032671478ad08a36a1
DEVESH PUNDIR <90322640+DeveshPundir@users.noreply.github.com>
2022-03-07 20:01:54 +0530

    Update Problem3.cpp

--- Practical/week 2/Problem3.cpp
+++ Practical/week 2/Problem3.cpp
@@ -18,7 +18,7 @@ int main(){
   cin>>key;
   for(int i=0;i<n;i++)
   {
-      for(int j=0;j<n/2;j++)
+      for(int j=i;j<n;j++)
       {
           if(i!=j&&abs(v[i]-v[j])==key)
             count++;
