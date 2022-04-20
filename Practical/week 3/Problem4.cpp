a625b1bafa05f56696a88a19a5d0cc1f29bd3d8d
DEVESH PUNDIR <90322640+DeveshPundir@users.noreply.github.com>
2022-04-20 15:43:39 +0530

    Update Problem4.cpp

--- Practical/week 3/Problem4.cpp
+++ Practical/week 3/Problem4.cpp
@@ -3,41 +3,85 @@
 //(use sorting) (Time Complexity = O(n log n))
 #include<iostream>
 using namespace std;
-void insertion_sort(int a[],int n)
+void merge(int a[], int l, int m, int r)
 {
-    int count=0;
-    for(int i=1;i<n;i++)
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int le[30];
+    int ri[30];
+    for (int i = 0; i < n1; i++)
     {
-         int c=a[i];
-         int j=i-1;
-         while(c<a[j]&&j>=0)
-         {
-            count++;
-             a[j+1]=a[j];
-             j--;
-
-         }
-         a[j+1]=c;
-
+        le[i] = a[l + i];
     }
-    cout<<count<<endl;
+    for (int i = 0; i < n2; i++)
+    {
+        ri[i] = a[m + 1 + i];
+    }
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2)
+    {
+        
+        if (le[i] < ri[j])
+        {
+            a[k] = le[i];
+            i++;
+            k++;
+        }
+        else
+        {
+            a[k] = ri[j];
+            k++;
+            j++;
+        }
+    }
+    while (i < n1)
+    {
+        a[k] = le[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        a[k] = ri[j];
+        j++;
+        k++;
+    }
+}
+void mergesort(int a[], int l, int r)
+{
+    if (l < r)
+    {
+        int mid = l + (r - l) / 2;
+        mergesort(a, l, mid);
+        mergesort(a, mid + 1, r);
+        merge(a, l, mid, r);
+    }
 }
 void check_duplicate(int a[],int n)
 {
-    int i=0,j=n-1;
-     while(i<j)
+    int i=0,j=n-1,f=0;
+     for(int i=0;i<n-1;i++)
      {
-         int mid=i+(j-i)/2;
-         if(a[mid]==a[mid+1]||a[mid]==a[mid-1])
+         
+         if(a[i]==a[i+1])
          {
-             cout<<"YES";
-             return;
+             f=1;
+            
+             break;
          }
          
      }
+     if(f==1)
+     cout<<"YES"<<endl;
+     else
+     cout<<"No"<<endl;
 }
 int main(){
- int a[30];
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a[30];
   int n;
   
   cout<<"Enter the no. of elements to be stored"<<endl;
@@ -48,11 +92,9 @@ int main(){
       cin>>x;
       a[i]=x;
   }
-  insertion_sort(a,n);
+  mergesort(a,0,n-1);
   check_duplicate(a,n);
-  for(int i=0;i<n;i++)
-  {
-      cout<<a[i]<<" ";
-  }
+  
+    }
 return 0;
 }
