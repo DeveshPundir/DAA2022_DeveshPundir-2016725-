ba696f228b8a3a6166460b29daa0783c4f519120
DEVESH PUNDIR <90322640+DeveshPundir@users.noreply.github.com>
2022-04-20 14:44:10 +0530

    Update Problem2.cpp

--- Practical/week 2/Problem2.cpp
+++ Practical/week 2/Problem2.cpp
@@ -1,46 +1,57 @@
 //Given a sorted array of +ve int .find 3 indexes i,j,k such that array[i]+array[j]=array[k]
-#include<iostream>
-#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
-int main(){
-  int t;
-  cin>>t;
-  while(t--)
-  {  vector<int> v;
-  int n;
-  cout<<"FIRST ENTER THE NO. OF ELEMENTS TO ENTERED THEN ENTER THE SORTED ARRAY ELEMENTS"<<endl;
-  cin>>n;
-  for(int i=0;i<n;i++)
-  {
-      int a;
-      cin>>a;
-      v.push_back(a);
 
-  }
-  int i,j,k,f=0;
-  for(int k=n-1;k>=0;k--)
-  {
-      int sum=v[k];
-      int i=0,j=k-1;
-      while(i<j)
-      {
-          if(v[i]+v[j]==sum)
-          {
-            f=1;
-              cout<<i<<"," <<j<<","<<k<<endl;
-              i++;
-              j--;
-          }
-          else if( sum>v[i]+v[j])
-           i++;
-           else
-           j--;
-      }
-  }
-    if(f==0)
-      cout<<"No sequence Found"<<endl;
-  
-  }
 
-return 0;
+int find_index(int a[],int n)
+{
+    int aa,b,c;
+    bool ans = false;
+
+    for(int i=0;i<n-2;i++)
+    {
+        for(int j=i+1;j<n-1;j++)
+        {
+            for(int k=j+1;k<n;k++)
+            {
+                if(a[i]+a[j]==a[k])
+                {
+                    aa=i+1;
+                    b=j+1;
+                    c=k+1;
+                   
+                    cout<<aa<<" "<<b<<" "<<c<<endl;
+                }
+            }
+        }
+    }
+
+    if(!ans)
+    cout<<"No sequence found!!"<<endl;
+
+    return 0;
+
+}
+
+
+int main()
+{
+    int t;cin>>t;
+
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        int a[n];
+        
+        for (int i = 0; i < n; i++)
+        {
+            cin>> a[i];
+        }
+
+        find_index(a,n);
+        
+        return 0;
+    }
 }
