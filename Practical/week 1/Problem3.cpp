a0795c9e1e5173933c9c71fd5e0ce610a3109f7c
DEVESH PUNDIR <90322640+DeveshPundir@users.noreply.github.com>
2022-04-20 13:19:17 +0530

    Create Problem3.cpp

---
+++ Practical/week 1/Problem3.cpp
@@ -0,0 +1,46 @@
+//binary search
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+   
+   int t;
+   cin>>t;
+   while(t--)
+   {
+       
+       int n,count=0;
+       cin>>n;
+       vector<int>v (n,0);
+       for(int i=0;i<n;i++)
+       {
+        cin>>v[i];   
+       }
+       int key;
+       cin>>key;
+       int i=0,j=n-1,f=0;
+       while(i<j)
+       {
+           int mid=(i+j)/2;
+           if(v[mid]==key)
+           {
+               f=1;
+               cout<<count<<"comparisons and present"<<endl;
+               break;
+           }
+           count++;
+           if(v[mid]>key)
+           j=mid;
+           else
+           i=mid+1;
+       }
+       if(f==0)
+       cout<<"Not present"<<endl;
+       
+       
+   }
+
+    return 0;
+}
